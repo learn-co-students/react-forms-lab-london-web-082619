4ff73733b225ae1d3500940d52eb625cefbdb66a
ck2e14 <chriswkennedy@icloud.com>
2019-11-04 14:25:13 +0000

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,15 +4,29 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+
+    // here we have the same name for the state key ('message') as we do for the name of the input
+    // in the form itself. That way, we can have a generic 'handleChange' function (below) that 
+    // will appropriately update state. 
+
+    this.state = {
+      message: "test state"
+    };
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.message} onChange={event => this.handleChange(event)} />
+      <div>{this.props.maxChars - this.state.message.length}</div>
       </div>
+
     );
   }
 }
